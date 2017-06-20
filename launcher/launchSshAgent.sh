01c6ceb5e77cc1c0a7b141a4bc475b48a1bd9c2a
Alessandro <alessandro.ivaldi@email.it>
2017-06-20 09:16:02 +0200

    Modified the ssh agent launcher

--- launcher/launchSshAgent.sh
+++ launcher/launchSshAgent.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-eval "$(ssh-agent -s)"
+#Start ssh-agent if there isn't already one running
+if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+    ssh-agent > ~/.ssh-agent-lock
+fi
+if [[ "$SSH_AGENT_PID" == "" ]]; then
+    eval "$(<~/.ssh-agent-lock)"
+fi
+ 
